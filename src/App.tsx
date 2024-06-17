725a2be4467495e17d33268fd33d32e358fdf276
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-06-17 17:19:56 +0530

    created patient home ongoing appointment component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { AiFillAudio } from "react-icons/ai";
 import { Button, message } from "antd";
+import LeftSection from "./components/patient/home/LeftSection";
 
 function App() {
-  const [messageApi, contextHolder] = message.useMessage();
-
-  const info = () => {
-    messageApi.info("Hello, Ant Design!");
-  };
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <br />
-      <AiFillAudio />
-      {contextHolder}
-      <Button type="primary" onClick={info}>
-        Display normal message
-      </Button>
+      <LeftSection />
     </>
   );
 }
---
+++ src/components/patient/home/LeftSection.tsx
@@ -0,0 +1,35 @@
+import ongoingAppointment from "./../../../assets/images/patient/home/ongoing_appointment.png";
+
+const LeftSection = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-between w-3/5 min-h-[550px] rounded-[16px] bg-[#ffffff] p-[20px]">
+        <h3 className="text-[#151515]">
+          <b>Ongoing Appointment</b>
+        </h3>
+
+        <div className="flex justify-center">
+          <img
+            className="w-60 h-56"
+            src={ongoingAppointment}
+            alt="Ongoing-appointment-image"
+          />
+        </div>
+
+        <div className=" flex text-center  w-full justify-center">
+          <div className="w-96 ">
+            <p className="text-[#151515] mb-6">
+              You have no ongoing appointments at the moment
+            </p>
+            <p className="text-[#868686]">
+              Once the receptionist accepts and confirms your payment, the
+              appointment details will be displayed here.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LeftSection;
