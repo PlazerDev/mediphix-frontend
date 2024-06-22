215cfe875dc9770b9f5b5838f3a066fac550147d
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-06-22 18:58:17 +0530

    Patient Home page left section UI done

--- src/components/patient/home/LeftSection.tsx
+++ src/components/patient/home/LeftSection.tsx
@@ -1,31 +1,11 @@
-import ongoingAppointment from "./../../../assets/images/patient/home/ongoing_appointment.png";
+import InTheQueue from "./InTheQueue";
 
 const LeftSection = () => {
   return (
     <>
-      <div className="flex flex-col justify-between w-3/5 min-h-[550px] rounded-[16px] bg-[#ffffff] p-[20px]">
-        <h3 className="text-[#151515]">
-          <b>Ongoing Appointment</b>
-        </h3>
-
-        <div className="flex justify-center">
-          <img
-            className="w-60 h-56"
-            src={ongoingAppointment}
-            alt="Ongoing-appointment-image"
-          />
-        </div>
-
-        <div className=" flex text-center  w-full justify-center">
-          <div className="w-96 ">
-            <p className="text-[#151515] mb-6">
-              You have no ongoing appointments at the moment
-            </p>
-            <p className="text-[#868686]">
-              Once the receptionist accepts and confirms your payment, the
-              appointment details will be displayed here.
-            </p>
-          </div>
+      <div className="w-2/3 bg-[#E3E3E3]">
+        <div className="flex justify-center ml-4 p-8 bg-[#ffffff] rounded-[16px]">
+          <InTheQueue/>
         </div>
       </div>
     </>
--- src/components/patient/home/Token.tsx
+++ src/components/patient/home/Token.tsx
@@ -2,69 +2,77 @@ import qr from "./../../../assets/images/patient/home/qr_code.png";
 import bgimage from "./../../../assets/images/patient/home/token_image.png";
 import "./Token.css";
 
+import { IoDownloadOutline } from "react-icons/io5";
+
 const Token = () => {
   return (
     <>
-      <div className="max-w-[650px] bg-gradient-to-r from-[#DF640B] to-[#FF7300] rounded-[16px]">
-        <div
-          className="flex bgimage bg-contain bg-no-repeat bg-right"
-          style={{ backgroundImage: `url(${bgimage})` }}
-        >
-          <div className="flex  justify-between p-5 ">
-            <div className="flex flex-col ml-3">
-              <h2 className="text-[#FFFFFF] text-center  mt-2 mb-4 text-[24px]">
-                Scan Me
-              </h2>
+      <div className="flex flex-col  max-h-[230px]  min-w-[550px]">
+        <div className="max-w-[650px] bg-gradient-to-r from-[#DF640B] to-[#FF7300] rounded-[16px]">
+          <div
+            className="flex bgimage  max-h-[230px] bg-contain bg-no-repeat bg-right"
+            style={{ backgroundImage: `url(${bgimage})` }}
+          >
+            <div className="flex  justify-between py-3 pl-8 ">
+              <div className="flex flex-col ">
+                <h2 className="text-[#FFFFFF] text-center  mt-2 mb-4 text-xl font-semibold">
+                  Scan Me
+                </h2>
 
-              <img
-                className="w-36 h-36 rounded-[16px] p-1 bg-[#FFFFFF]"
-                src={qr}
-              />
-            </div>
-            <div className="flex items-center">
-              <div className="border-l-2 border-[#FFFFFF] ml-8 mr-4 h-24"></div>
-            </div>
-            <div className="text-white flex-1 ml-4 min-w-[235px]">
-              <div className="mb-2">
-              <h4 className="text-[#151515] text-base">
-                  <b>2024/06/17 |</b>
-                </h4>
-                <h4 className="text-[#151515] text-base">
-                  <b> 06.00PM - 07.00PM</b>
-                </h4>
+                <img
+                  className="w-32 h-32 rounded-[16px] p-1 bg-[#FFFFFF]"
+                  src={qr}
+                />
               </div>
-              <div className="mb-1 ">
-                <p className="text-sm">Name</p>
-                <h4 className="font-bold text-base">Vishwa Sandaruwan</h4>
+              <div className="flex items-center">
+                <div className="border-l-2 border-[#FFFFFF] mx-8  h-16"></div>
               </div>
-              <div className="mb-1">
-                <p className="text-sm">Doctor Name</p>
-                <h4 className="font-bold text-base">Dr. B.KARUNARATNE</h4>
-              </div>
-              <div className="mb-1">
-                <p className="text-sm">Medical Center</p>
-                <h4 className="font-bold text-base">
-                  Aloka Clinic & Laboratory
-                </h4>
-              </div>
-              <div className="mb-1">
-                <p className="text-sm">Queue Number</p>
-                <div className="flex justify-center items-center mt-1 w-8 h-8 bg-[#FFFFFF] rounded-[8px]">
-                  <h4 className="font-bold text-base text-[#151515]">07</h4>
+              <div className="text-white min-w-[250px]">
+                <div className="mb-1">
+                  <h4 className="text-[#151515] text-[14px] font-semibold ">
+                    2024/06/17 | 06.00PM - 07.00PM
+                  </h4>
+                </div>
+                <div className="mb-1 ">
+                  <p className="text-[12px]">Name</p>
+                  <h4 className="font-semibold text-[14px]">
+                    Vishwa Sandaruwan
+                  </h4>
+                </div>
+                <div className="mb-1">
+                  <p className="text-[12px]">Doctor Name</p>
+                  <h4 className="font-semibold text-[14px]">
+                    Dr. B.KARUNARATNE
+                  </h4>
+                </div>
+                <div className="mb-1">
+                  <p className="text-[12px]">Medical Center</p>
+                  <h4 className="font-semibold text-[14px]">
+                    Aloka Clinic & Laboratory
+                  </h4>
+                </div>
+                <div className="">
+                  <p className="text-[14px]">Queue Number</p>
+                  <div className="flex justify-center items-center mt-1 w-6 h-6 bg-[#FFFFFF] rounded-[8px]">
+                    <h4 className="font-bold text-[14px] text-[#151515]">07</h4>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="  flex text-[#FFFFFF] items-end">
-            <p className="bg-[#363636] px-11 py-2 [border-radius:16px_0_16px_0] ">
-              REF_8576
-            </p>
+            <div className="  flex text-[#FFFFFF] items-end">
+              <p className="bg-[#363636] px-8 py-2 [border-radius:16px_0_16px_0] ml-4">
+                REF_8056
+              </p>
+            </div>
           </div>
         </div>
+        <div className="flex justify-center text-[#FF7300]">
+          <IoDownloadOutline className="font-extrabold text-lg mt-1 mr-1" />
 
-        {/* <div>
-          <img className="bgimage w-36 h-56 object-cover" src={bgimage} />
-        </div> */}
+          <p className=" text-center pt-1 font-bold text-[14px]">
+            Download Token Now
+          </p>
+        </div>
       </div>
     </>
   );
