9a999c48975c9a9da9099f1eea041cc029f18948
Sajinimalsha <2021is021@stu.ucsc.cmb.ac.lk>
2024-06-22 11:52:28 +0530

    Create patient appointment table file

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import LoginOtp from './components/login/patient/LoginOtp'
 import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom'
 import Home from './components/Home'
 import MedicalLogin from './components/login/medical-center/MedicalLogin'
+import PatientAppointmentTable from './components/login/patient/PatientAppointmentTable'
+
+
 
 function App() {
   return (
@@ -11,18 +14,21 @@ function App() {
         <Router>
           <Routes>
 
-            <Route path='/' element={<Home/>}/ >
-            <Route path='/Login' element={<Login/>}/ >
+            <Route path='/' element={<Home/>}/>
+            <Route path='/Login' element={<Login/>}/>
             <Route path='/LoginOTP' element={  <LoginOtp/>}/>
             <Route path='/MedicalOfficialLogin' element={  <MedicalLogin/>}/>
-           
+            <Route path='/PatientAppointmentTable' element={  <PatientAppointmentTable/>}/>
+          
       
           </Routes>
 
         </Router>
+
     </div>
   )
 
 }
+
 
 export default App
