4be4b4b092f4576d207374a94c04d4f8097a2527
R.M.S.D.Rathnayaka <dhanarathnayake99@gmail.com>
2024-06-23 14:30:18 +0530

    Add another p tag for testing 4

--- src/components/patient/home/AppointmentCard.tsx
+++ src/components/patient/home/AppointmentCard.tsx
@@ -14,6 +14,7 @@ const AppointmentCard = () => {
           <p className="font-[400]">Aloka Clinic & Laboratory</p>
           <p className="font-[400]">Aloka Clinic & Laboratory 2</p>
           <p className="font-[400]">Aloka Clinic & Laboratory 3</p>
+          <p className="font-[400]">Aloka Clinic & Laboratory 4</p>
         </div>
       </div>
     </>
