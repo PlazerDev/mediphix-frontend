dce83d06737186ed1f4ae3ae3918b134199334b4
Sajinimalsha <2021is021@stu.ucsc.cmb.ac.lk>
2024-06-23 18:16:20 +0530

    Previous appointment UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-
-
 import Login from './components/login/patient/Login'
 import LoginOtp from './components/login/patient/LoginOtp'
 import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom'
 import Home from './components/Home'
 import MedicalLogin from './components/login/medical-center/MedicalLogin'
-import PatientAppointmentTable from './components/login/patient/PatientAppointmentTable'
 import AppointmentSection from './components/patient/appointment/AppointmentSection'
 import PreviousAppointments from './components/patient/appointment/PreviousAppointments'
+
 
 
 function App() {
@@ -18,21 +16,15 @@ function App() {
 
             <Route path='/' element={<Home/>}/>
             <Route path='/Login' element={<Login/>}/>
-            <Route path='/LoginOTP' element={  <LoginOtp/>}/>
-
-          
-            <Route path='/PatientAppointmentTable' element={  <PatientAppointmentTable/>}/>
-
+            <Route path='/LoginOTP' element={  <LoginOtp/>}/>  
             <Route path='/MedicalOfficialLogin' element={  <MedicalLogin/>}/> 
-            <Route path='/home' element={  <PatientNavigation/>}/>
             <Route path='/appoinments' element={  <AppointmentSection/>}/>
-            <Route path='/previousappoinments' element={  <PreviousAppointments/>}/>
+            <Route path='/PreviousAppointments' element={<PreviousAppointments/>}/>
 
            
 
       
           </Routes>
-
         </Router>
 
     </div>
