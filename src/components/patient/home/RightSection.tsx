b5ecda5876a3e4401eb9d1fde1c44085f20a777e
R.M.S.D.Rathnayaka <dhanarathnayake99@gmail.com>
2024-06-23 13:00:14 +0530

    Implement No Appointment section.

--- src/components/patient/home/RightSection.tsx
+++ src/components/patient/home/RightSection.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import UpcomingAppointment from "./UpcomingAppointment ";
 import ProfileOverview from "./ProfileOverview";
+import NoUpcomingAppointment from "./NoUpcomingAppointment";
 
 const RightSection = () => {
   return (
     <div className="w-1/3 flex flex-col gap-4 p-8">
-      <UpcomingAppointment />
-      <ProfileOverview />
+        <NoUpcomingAppointment/>
+        <ProfileOverview />
     </div>
   );
 };
