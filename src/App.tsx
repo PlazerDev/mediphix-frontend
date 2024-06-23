a454fc09d670a123d6f2c0669412224e597f5e76
R.M.S.D.Rathnayaka <dhanarathnayake99@gmail.com>
2024-06-23 15:12:17 +0530

    Started creating previous appointment details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,29 @@
-
-
-import Login from './components/login/patient/Login'
-import LoginOtp from './components/login/patient/LoginOtp'
-import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom'
-import Home from './components/Home'
-import MedicalLogin from './components/login/medical-center/MedicalLogin'
-import PatientAppointmentTable from './components/login/patient/PatientAppointmentTable'
-import AppointmentSection from './components/patient/appointment/AppointmentSection'
-import PreviousAppointments from './components/patient/appointment/PreviousAppointments'
-
+import Login from "./components/login/patient/Login";
+import LoginOtp from "./components/login/patient/LoginOtp";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import MedicalLogin from "./components/login/medical-center/MedicalLogin";
+import PatientAppointmentTable from "./components/login/patient/PatientAppointmentTable";
+import AppointmentSection from "./components/patient/appointment/AppointmentSection";
+import PreviousAppointments from "./components/patient/appointment/PreviousAppointments";
 
 function App() {
   return (
     <div>
-        <Router>
-          <Routes>
-
-            <Route path='/' element={<Home/>}/>
-            <Route path='/Login' element={<Login/>}/>
-            <Route path='/LoginOTP' element={  <LoginOtp/>}/>
-
-          
-            <Route path='/PatientAppointmentTable' element={  <PatientAppointmentTable/>}/>
-
-            <Route path='/MedicalOfficialLogin' element={  <MedicalLogin/>}/> 
-            <Route path='/home' element={  <PatientNavigation/>}/>
-            <Route path='/appoinments' element={  <AppointmentSection/>}/>
-            <Route path='/previousappoinments' element={  <PreviousAppointments/>}/>
-
-           
-
-      
-          </Routes>
-
-        </Router>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/LoginOTP" element={<LoginOtp />} />
+          <Route path="/PatientAppointmentTable" element={<PatientAppointmentTable />} />
+          <Route path="/MedicalOfficialLogin" element={<MedicalLogin />} />
+          <Route path="/home" element={<PatientNavigation />} />
+          <Route path="/appoinments" element={<AppointmentSection />} />
+          <Route path="/previousappoinments" element={<PreviousAppointments />}/>
+        </Routes>
+      </Router>
     </div>
-  )
-
+  );
 }
 
-
-export default App
+export default App;
