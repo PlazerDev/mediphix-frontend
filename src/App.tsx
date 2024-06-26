15b1f886dcfa30e848c4db5d475adb79031bee47
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-06-26 11:37:00 +0530

    Completed recordbook solid ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,40 @@
-
-
-import Login from './components/login/patient/Login'
-import LoginOtp from './components/login/patient/LoginOtp'
-import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom'
-import Home from './components/Home'
-import MedicalLogin from './components/login/medical-center/MedicalLogin'
-
-import PatientNavigation from "./components/patient/navigation/PatientNavigation"
-import AppointmentSection from './components/patient/appointment/AppointmentSection'
-import PatientHome from './components/patient/home/PatientHome'
-
-import PatientAppointmentTable from './components/login/patient/PatientAppointmentTable'
-import AppointmentSection from './components/patient/appointment/AppointmentSection'
-import PreviousAppointments from './components/patient/appointment/PreviousAppointments'
-
+import Login from "./components/login/patient/Login";
+import LoginOtp from "./components/login/patient/LoginOtp";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import MedicalLogin from "./components/login/medical-center/MedicalLogin";
+import PatientNavigation from "./components/patient/navigation/PatientNavigation";
+import AppointmentSection from "./components/patient/appointment/AppointmentSection";
+import PatientHome from "./components/patient/home/PatientHome";
+import RecordBook from "./components/patient/recordbook/RecordBook";
+import PatientAppointmentTable from "./components/login/patient/PatientAppointmentTable";
+import PreviousAppointments from "./components/patient/appointment/PreviousAppointments";
 
 function App() {
   return (
     <div>
-        <Router>
-          <Routes>
-
-            <Route path='/' element={<Home/>}/>
-            <Route path='/Login' element={<Login/>}/>
-            <Route path='/LoginOTP' element={  <LoginOtp/>}/>
-
-          
-            <Route path='/PatientAppointmentTable' element={  <PatientAppointmentTable/>}/>
-
-            <Route path='/MedicalOfficialLogin' element={  <MedicalLogin/>}/> 
-            <Route path='/home' element={  <PatientNavigation/>}/>
-            <Route path='/appoinments' element={  <AppointmentSection/>}/>
-
-            <Route path='/patienthome' element={  <PatientHome/>}/>
-
-            <Route path='/previousappoinments' element={  <PreviousAppointments/>}/>
-
-
-           
-
-      
-          </Routes>
-
-        </Router>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/LoginOTP" element={<LoginOtp />} />
+          <Route
+            path="/PatientAppointmentTable"
+            element={<PatientAppointmentTable />}
+          />
+          <Route path="/MedicalOfficialLogin" element={<MedicalLogin />} />
+          <Route path="/home" element={<PatientNavigation />} />
+          <Route path="/appoinments" element={<AppointmentSection />} />
+          <Route path="/patienthome" element={<PatientHome />} />
+          <Route path="/recordbook" element={<RecordBook />} />
+          <Route
+            path="/previousappoinments"
+            element={<PreviousAppointments />}
+          />
+        </Routes>
+      </Router>
     </div>
-  )
-
+  );
 }
 
-
-export default App
+export default App;
