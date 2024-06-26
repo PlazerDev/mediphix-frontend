27900ee5bc86e72cb5bdf2f56674cdb0cd91e9bc
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-06-26 11:00:57 +0530

    login segment working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 
-
-import Login from './components/login/patient/Login'
 import LoginOtp from './components/login/patient/LoginOtp'
 import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom'
 import Home from './components/Home'
@@ -8,6 +6,7 @@ import MedicalLogin from './components/login/medical-center/MedicalLogin'
 import PatientAppointmentTable from './components/login/patient/PatientAppointmentTable'
 import AppointmentSection from './components/patient/appointment/AppointmentSection'
 import PreviousAppointments from './components/patient/appointment/PreviousAppointments'
+import LoginAuth from './components/login/LoginAuth'
 
 
 function App() {
@@ -17,14 +16,13 @@ function App() {
           <Routes>
 
             <Route path='/' element={<Home/>}/>
-            <Route path='/Login' element={<Login/>}/>
+            <Route path='/Login' element={<LoginAuth/>}/>
             <Route path='/LoginOTP' element={  <LoginOtp/>}/>
-
-          
+            
             <Route path='/PatientAppointmentTable' element={  <PatientAppointmentTable/>}/>
 
             <Route path='/MedicalOfficialLogin' element={  <MedicalLogin/>}/> 
-            <Route path='/home' element={  <PatientNavigation/>}/>
+     
             <Route path='/appoinments' element={  <AppointmentSection/>}/>
             <Route path='/previousappoinments' element={  <PreviousAppointments/>}/>
 
--- src/components/login/patient/LoginOtp.tsx
+++ src/components/login/patient/LoginOtp.tsx
@@ -1,6 +1,6 @@
 
-// import loginBg from './../../../assets/images/patient/login/login-bg.png';
-import image1 from './../../../assets/images/patient/login/login2.jpg'
+
+import image1 from '../../../assets/images/patient/login/login2.jpg';
 import 'typeface-cookie';
 import { Segmented } from 'antd';
 import './../../../assets/css/login/custome__segment.css'
@@ -11,30 +11,33 @@ import { Link } from 'react-router-dom';
 function LoginOtp() {
   return (
     <div className='bg-[linear-gradient(211deg,_#FF8D30_0%,_#A74C00_100%)]   w-screen h-screen flex'>
-      {/* <div className="bg-[linear-gradient(211deg,_#FF8D30_0%,_#A74C00_100%)] w-1/2 h-screen bg-cover bg-no-repeat"> */}
-      <div className="bg-login-background1 w-1/2 h-screen bg-cover bg-no-repeat  ">
-        <div className='bg-[#FFE7D3] border-solid  rounded-[16px] w-[70%] h-[80%] left-[13%] top-[10%] bottom-[10%] relative flex flex-col ' >
-          <div className='h-[50%] flex flex-col text-center '>
-
-            <div className="flex-1 h-1/4 sm:h-auto  text-[24px] font-cookie mt-4">
+      
+      <div className="bg-login-background1 w-1/2 h-screen bg-cover bg-no-repeat">
+        <div className='bg-[#FFE7D3] border-solid rounded-[16px] w-[70%] h-[80%] left-[13%] top-[10%] bottom-[10%] relative flex flex-col'>
+          <div className='h-[50%] flex flex-col text-center'>
+            <div className="flex-1 h-1/4 sm:h-auto text-[24px] font-cookie mt-4">
               Hello there ✌
             </div>
-            <div className="flex-1 h-1/2 sm:h-auto  text-[16px]">
+            <div className="flex-1 h-1/2 sm:h-auto text-[16px]">
               Which of the following <br /> describes you ?
             </div>
             <div className="flex-1 h-1/4 sm:h-auto mt-[10px] p-3 ">
-              <Segmented options={['Patient', 'Medical Center']}
-                style={{ backgroundColor: '#E3E3E3', color: '(var(--Text-C))' }}
-
-                className=' custom-segmented ' />
+              <Segmented
+                options={['Patient', 'Medical Center']}
+                // onChange=
+                defaultValue="Patient"
+                disabled
+                style={{ backgroundColor: '#E3E3E3', color: 'var(--Text-C)' }}
+                className='custom-segmented'
+              />
             </div>
           </div>
-
-          <div className='w-full h-[60%] '>
+          <div className='w-full h-[60%]'>
             <img src={image1} className='w-[100%] h-full object-fill rounded-b-[16px]' />
           </div>
         </div>
       </div>
+
 
 
       <div className='bg-white w-1/2 h-screen '>
@@ -76,7 +79,7 @@ function LoginOtp() {
 
             <div className='flex  mt-6'>
 
-              <button className=' cursor-pointer text-[#fff] text-center  rounded-[20px] w-full border  p-2  hover:bg-opacity-15  bg-[var(--accent)]  border-[var(--accent)] duration-400' >
+              <button className=' cursor-pointer text-[#fff] text-center  rounded-[20px] w-full border  p-2  hover:bg-orange-600  bg-[var(--accent)]  border-[var(--accent)] duration-400' >
                 Login
               </button>
 
