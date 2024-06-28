6c356b3061c01ebe0eb53e486a11c196005f6f3a
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-06-29 00:22:42 +0530

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,9 @@ import MedicalLogin from './components/login/medical-center/MedicalLogin'
 
 import PatientHome from './components/patient/home/PatientHome'
 
-import PatientAppointmentTable from './components/login/patient/PatientAppointmentTable'
 import AppointmentSection from './components/patient/appointment/AppointmentSection'
 import PreviousAppointments from './components/patient/appointment/PreviousAppointments'
 import LoginAuth from './components/login/LoginAuth'
-// import NavigationRouter from './components/patient/navigation/NavigationRouter'
 import PatientNavigation from './components/patient/navigation/PatientNavigation'
 import { useState } from 'react'
 import RecordBook from './components/patient/recordbook/RecordBook'
@@ -25,15 +23,13 @@ function App() {
     <div>
       <Router>
 
-      <PatientNavigation/>
+      {/* <PatientNavigation/> */}
 
         <Routes>
          
               <Route path='/' element={<Home />} />
               <Route path='/Login' element={<LoginAuth />} />
               <Route path='/LoginOTP' element={<LoginOtp />} />
-          
-              <Route path='/PatientAppointmentTable' element={<PatientAppointmentTable />} />
               <Route path='/MedicalOfficialLogin' element={<MedicalLogin />} />
               <Route path='/patient/appointment' element={<AppointmentSection />} />
               <Route path='/patient/home' element={<PatientHome />} />
