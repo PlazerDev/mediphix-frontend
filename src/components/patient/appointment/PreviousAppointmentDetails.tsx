c3d177fa7c6761dcfda476852327f27036467283
R.M.S.D.Rathnayaka <dhanarathnayake99@gmail.com>
2024-06-30 16:45:11 +0530

    Finish previous appointment details page

--- src/components/patient/appointment/PreviousAppointmentDetails.tsx
+++ src/components/patient/appointment/PreviousAppointmentDetails.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 import Logo from "./../../../assets/images/mediphix.jpg";
 import { Segmented } from "antd";
+import Footer from "./../../Footer";
 import {
   BookOutlined,
   HomeOutlined,
@@ -16,12 +17,12 @@ import { Breadcrumb } from "antd";
 const PreviousAppointmentDetails = () => {
   return (
     <div>
-      <div className="mt-4 ml-4">
+      <div className="mt-2 ml-4">
         <p className="font-Roboto font-[700] text-xl text-[#151515]">
           Appointment - REF_1653
         </p>
       </div>
-      <div className="mt-1 ml-4 mb-4">
+      <div className="mt-1 ml-4 mb-2">
         <Breadcrumb
           items={[
             {
@@ -36,97 +37,99 @@ const PreviousAppointmentDetails = () => {
           ]}
         />
       </div>
-      <div className="ml-4 mr-4 p-8 bg-[#ffffff] rounded-[16px] ">
-        <div className="flex w-full justify-between">
-          <div className="ml-4">
-            <p className="text-[#868686]">Reference Number</p>
+      <div className="ml-4 p-8 bg-[#ffffff] rounded-[16px] ">
+        <div className="w-full grid grid-cols-3 gap-4">
+          <div>
+            <p className="text-[#868686] text-sm">Reference Number</p>
             <p>REF_1653</p>
-            <p className="text-[#868686]">Queue Number</p>
+            <p className="text-[#868686] text-sm">Queue Number</p>
             <p>07</p>
           </div>
           <div>
-            <p className="text-[#868686]">Date</p>
+            <p className="text-[#868686] text-sm">Date</p>
             <p>2024/June/13</p>
-            <p className="text-[#868686]">Status</p>
-            <p className="text-[#FF7300]">Ended</p>
-            <div className="flex bg-[#FF7300] rounded-[8px] align-middle p-2 text-[#FFFFFF] w-fit ">
-                {" "}
-                <MdOutlineRemoveRedEye className="text-lg mr-1" />
-                <p className="text-sm ">View Record Book Entry</p>
+            <div className="flex">
+              <div className="flex-col mr-4">
+                <p className="text-[#868686] text-sm">Status</p>
+                <p className="text-[#FF7300]">Ended</p>
               </div>
+              <div className="flex bg-[#FF7300] rounded-[8px] align-middle px-4 py-2 text-[#FFFFFF] w-fit ">
+                <div className="flex my-1">
+                  <MdOutlineRemoveRedEye className="text-lg mr-1" />
+                  <p className="text-sm ">View Record Book Entry</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div>
-            <p className="text-[#868686]">Time Slot</p>
+            <p className="text-[#868686] text-sm">Time Slot</p>
             <p>03.00 PM - 04.00 PM</p>
           </div>
         </div>
-        <div>
         <hr className="my-4" />
-        <h3 className="text-lg mb-3 font-semibold">Payment Details</h3>
-        <div className="flex w-full ">
-          <div className="w-96 mr-60">
-            <p className="text-[#868686]">Payment Date</p>
+
+        <h3 className="text-base mb-2 font-semibold">Payment Details</h3>
+        <div className=" w-full grid grid-cols-3 gap-4">
+          <div>
+            <p className="text-[#868686] text-sm">Payment Date</p>
             <p>2024/June/15</p>
           </div>
-          <div>
-            <p className="text-[#868686]">Payment Time</p>
+          <div className="col-span-2">
+            <p className="text-[#868686] text-sm">Payment Time</p>
             <p>03.00 PM - 04.00 PM</p>
-          </div>
           </div>
         </div>
 
-        <div>
         <hr className="my-4" />
-        <h3 className="text-lg mb-3 font-semibold">Medical Center Details</h3>
-        <div className="flex w-full justify-between">
+        <h3 className="text-base mb-2 font-semibold">Medical Center Details</h3>
+        <div className="w-full grid grid-cols-3 gap-4n">
           <div>
-            <p className="text-[#868686]">Medical Center Name</p>
-            <p className="text-[#FF7300]">Aloka Medical Center</p>
+            <p className="text-[#868686] text-sm">Medical Center Name</p>
+            <p className="text-[#FF7300] underline">Aloka Medical Center</p>
           </div>
           <div>
-            <p className="text-[#868686]">Contact Number</p>
+            <p className="text-[#868686] text-sm">Contact Number</p>
             <p>011-4600600</p>
           </div>
           <div>
-            <p className="text-[#868686]">Location</p>
+            <p className="text-[#868686] text-sm">Location</p>
             <p>No: 10/A, Queens Rd, Colombo 7</p>
           </div>
-          </div>
         </div>
 
-        <div>
         <hr className="my-4" />
-        <h3 className="text-lg mb-3 font-semibold">Doctor Details</h3>
-        <div className="flex w-full justify-between">
+
+        <h3 className="text-base mb-2 font-semibold">Doctor Details</h3>
+        <div className="w-full grid grid-cols-3 gap-4">
           <div>
-            <p className="text-[#868686]">Doctor's Name</p>
-            <p className="text-[#FF7300]">Dr. C.M.K Jayawardanar</p>
+            <p className="text-[#868686] text-sm">Doctor's Name</p>
+            <p className="text-[#FF7300] underline">Dr. C.M.K Jayawardana</p>
           </div>
           <div>
-            <p className="text-[#868686]">Category</p>
+            <p className="text-[#868686] text-sm">Category</p>
             <p>OPD Doctor</p>
           </div>
-          </div>
         </div>
 
-        <div>
         <hr className="my-4" />
-        <h3 className="text-lg mb-3 font-semibold">Session Details</h3>
-        <div className="flex w-full justify-between">
+        <h3 className="text-base mb-2 font-semibold">Session Details</h3>
+        <div className="w-full grid grid-cols-3 gap-4">
           <div>
-            <p className="text-[#868686]">Started date & time</p>
-            <p className="text-[#FF7300]">2024/June/15 - 03.42 PM</p>
+            <p className="text-[#868686] text-sm">Started date & time</p>
+            <p>2024/June/15 - 03.42 PM</p>
           </div>
           <div>
-            <p className="text-[#868686]">End date & time</p>
+            <p className="text-[#868686] text-sm">End date & time</p>
             <p>2024/June/15 - 03.51 PM</p>
           </div>
           <div>
-            <p className="text-[#868686]">Session Duration</p>
+            <p className="text-[#868686] text-sm">Session Duration</p>
             <p>09 Minutes</p>
           </div>
-          </div>
         </div>
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   );
