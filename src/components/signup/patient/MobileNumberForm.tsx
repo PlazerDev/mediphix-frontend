ba55ade99b4d0c69c1437276b2e21f419d8e6ec1
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-07-08 18:56:32 +0530

    patient signup done using states.

--- src/components/signup/patient/MobileNumberForm.tsx
+++ src/components/signup/patient/MobileNumberForm.tsx
@@ -1,24 +1,30 @@
+import React from 'react';
 
-
-function MobileNumberForm() {
+function MobileNumberForm(props) {
     return (
         <div className="">
             <p className='text-[var(--text-c)] mt-5'>
-                Enter your mobile phone number. During the verification phase, you will receive an OTP code for this mobile number
+                Enter your mobile phone number. During the verification phase, you will receive an OTP code for this mobile number.
             </p>
 
             <div className='mt-10'>
                 <span>Mobile Number: <span className='text-red-500'>*</span></span>
-                <div className='flex rounded-[20px] w-[50%] border border-gray-300  p-2 transition-colors duration-300 hover:border-[var(--accent)] '>
-                    <p className='text-[#868686]'>+94</p>
-                    <input type="text" className='rounded-[20px] focus:outline-none pl-2' placeholder='Enter Your Number' />
-                </div>
-                
-                
-
+                <form onSubmit={(e)=>{e.preventDefault}}>
+                    <div className='flex rounded-[20px] w-[50%] border border-gray-300 p-2 transition-colors duration-300 hover:border-[var(--accent)]'>
+                        <p className='text-[#868686]'>+94</p>
+                        <input
+                            type="text"
+                            name="mobileNumber"
+                            className='rounded-[20px] focus:outline-none pl-2'
+                            placeholder='Enter Your Number'
+                            value={props.formData.mobile}
+                            onChange={props.handleChange}
+                        />
+                    </div>
+                </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default MobileNumberForm
+export default MobileNumberForm;
