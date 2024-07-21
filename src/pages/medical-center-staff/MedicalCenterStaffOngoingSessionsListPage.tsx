9953a996dd75b47314ac9a9a9b34a7aa694eddb3
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-07-21 14:19:05 +0530

    added Sweet Alerts Theme Configurations

--- src/pages/medical-center-staff/MedicalCenterStaffOngoingSessionsListPage.tsx
+++ src/pages/medical-center-staff/MedicalCenterStaffOngoingSessionsListPage.tsx
@@ -2,8 +2,20 @@ import Footer from "./../../components/Footer";
 import MCSMainGreeting from "../../components/medical-center-staff/MCSMainGreeting";
 import MCSNavBar from "../../components/medical-center-staff/MCSNavBar";
 import MCSDateTimeTitle from "../../components/medical-center-staff/MCSDateTimeTitle";
+import swal from "sweetalert";
 
 function MedicalCenterStaffOnGoingSessionsListPage() {
+  swal({
+    title: "Attention",
+    text: "You have 2 ongoing clinic sessions",
+    icon: "info",
+    buttons: {
+      confirm: {
+        text: "OK",
+      },
+    },
+  });
+
   return (
     <div className="min-h-screen flex flex-col">
       {/* Navigation Bar  */}
