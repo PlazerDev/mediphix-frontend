aa796f589a6c80aa5f22870b9d2beb5063c5559a
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-07-21 23:39:51 +0530

    made ongoing session card mobile responsive

--- src/components/medical-center-staff/MCSOnGoingSessionDetailsCard.tsx
+++ src/components/medical-center-staff/MCSOnGoingSessionDetailsCard.tsx
@@ -23,29 +23,33 @@ function MCSOnGoingSessionDetailsCard({
     <Link to="#">
       <div className="bg-mediphix_card_background my-4 rounded-lg p-8 hover:shadow-md">
         <p className="font-bold">Clinic Session Details</p>
-        <div className="flex justify-between my-2">
-          <div className="flex-1">
-            <p className="text-mediphix_text_c text-sm">Time Frame</p>
-            <p>{timeFrame}</p>
+        <div className="my-2 flex md:flex-row flex-col">
+          <div className="flex flex-1">
+            <div className="flex-1">
+              <p className="text-mediphix_text_c text-sm">Time Frame</p>
+              <p>{timeFrame}</p>
+            </div>
+            <div className="flex-1">
+              <p className="text-mediphix_text_c text-sm">Date</p>
+              <p>{date}</p>
+            </div>
           </div>
-          <div className="flex-1">
-            <p className="text-mediphix_text_c text-sm">Date</p>
-            <p>{date}</p>
-          </div>
-          <div className="flex-1">
-            <p className="text-mediphix_text_c text-sm">Hall Number</p>
-            <p>{hallNumber}</p>
-          </div>
-          <div className="flex-1">
-            <p className="text-mediphix_text_c text-sm">Status</p>
-            <p className="text-mediphix_accent">{status}</p>
+          <div className="flex flex-1">
+            <div className="flex-1">
+              <p className="text-mediphix_text_c text-sm">Hall Number</p>
+              <p>{hallNumber}</p>
+            </div>
+            <div className="flex-1">
+              <p className="text-mediphix_text_c text-sm">Status</p>
+              <p className="text-mediphix_accent">{status}</p>
+            </div>
           </div>
         </div>
         <p className="font-bold mb-2">Doctor Details</p>
         <div className="flex gap-2">
           <div>
             <img
-              className="object-cover w-24 h-24 rounded-lg"
+              className="object-cover w-24 h-full rounded-lg"
               src={doctorImg}
             />
           </div>
