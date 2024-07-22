0b8f9b0519bbbf2ee42eb4e7fbdeefb97f94b955
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-07-22 10:15:29 +0530

    added steps navigation from antD

--- src/components/medical-center-staff/MCSTimeSlotCard.tsx
+++ src/components/medical-center-staff/MCSTimeSlotCard.tsx
@@ -1,3 +1,6 @@
+import { Steps } from "antd";
+import { useState } from "react";
+
 interface Prop {
   timeRange: string;
   maxPatientSessions: number;
@@ -5,9 +8,40 @@ interface Prop {
 }
 
 function MCSTimeSlotCard({ timeRange, maxPatientSessions, status }: Prop) {
+  const [current, setCurrent] = useState(0);
+
+  const onChange = (value: number) => {
+    console.log("onChange:", value);
+    setCurrent(value);
+  };
+
   return (
     <div className="bg-mediphix_card_background rounded-lg p-8">
-      <p className="font-bold">Clinic Session Details</p>
+      <Steps
+        type="navigation"
+        size="small"
+        current={current}
+        onChange={onChange}
+        className="site-navigation-steps mb-4"
+        items={[
+          {
+            title: "Time Slot 1",
+            status: "finish",
+            description: "08.00 AM - 09.00 AM",
+          },
+          {
+            title: "Time Slot 2",
+            status: "process",
+            description: "09.00 AM - 10.00 AM",
+          },
+          {
+            title: "Time Slot 3",
+            status: "wait",
+            description: "10.00 AM - 11.00 AM",
+          },
+        ]}
+      />
+      <p className="font-bold">Time Slot Details</p>
       <div className="my-2 flex md:flex-row flex-col gap-2 md:gap-0">
         <div className="flex-1">
           <p className="text-mediphix_text_c text-sm">Start Time - End Time </p>
