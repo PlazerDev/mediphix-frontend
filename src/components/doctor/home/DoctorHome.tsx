d4f0fa4bf2c31ac3597b97a0e54f9f68b0db0b80
R.M.S.D.Rathnayaka <dhanarathnayake99@gmail.com>
2024-07-23 17:03:04 +0530

    Add calendar to doctor home page

--- src/components/doctor/home/DoctorHome.tsx
+++ src/components/doctor/home/DoctorHome.tsx
@@ -1,9 +1,26 @@
 import Footer from "./../../Footer";
 import DoctorTimeSlots from "./DoctorTimeSlots";
-import NoOngoingSession from "./NoOngoingSession";
+//import NoOngoingSession from "./NoOngoingSession";
 import OngoingSessionData from "./OngoingSessionData";
+import { Calendar, theme } from "antd";
+import type { CalendarProps } from "antd";
+import type { Dayjs } from "dayjs";
+
+const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>["mode"]) => {
+  console.log(value.format("YYYY-MM-DD"), mode);
+};
+
+
+
 
 const DoctorHome = () => {
+    const { token } = theme.useToken();
+  
+    const wrapperStyle: React.CSSProperties = {
+      width: "100%",
+      border: `1px solid ${token.colorBorderSecondary}`,
+      borderRadius: token.borderRadiusLG,
+    };
   return (
     <>
       <div className="mt-2 ml-4">
@@ -25,8 +42,29 @@ const DoctorHome = () => {
               <p className="text-[#FF7300] text-sm font-semibold">View More</p>
             </div>
           </div>
+          <div className="m-4">
+          <div style={wrapperStyle}>
+            <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+          </div>
+           
+        </div>
           <DoctorTimeSlots />
           <DoctorTimeSlots />
+        </div>
+      </div>
+<div></div>
+      <div className="w-1/3 bg-[#fff] rounded-[16px] p-4 mr-4">
+        <div className=" flex justify-between">
+          <div>
+            <h1 className="text-lg font-bold">Ongoing Session</h1>
+          </div>
+
+          <div className="mt-1">
+            <p className="text-[#FF7300] text-sm font-semibold">View More</p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center bg-contain">
+           <OngoingSessionData />
         </div>
       </div>
 
