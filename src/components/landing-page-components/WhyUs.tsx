c50c3f2f0ec2c92951790aac7d9f08887adb693f
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-07-29 19:55:39 +0530

    implemented features

--- src/components/landing-page-components/WhyUs.tsx
+++ src/components/landing-page-components/WhyUs.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 function WhyUs() {
-  return <div>WhyUs</div>;
+  return (
+    <div className="bg-mediphix_text_a text-white">
+      <p>Why Us?</p>
+      <p>
+        Mediphix puts your healthcare needs at your fingertips. Easily book
+        appointments, access your medical records, and stay informed with timely
+        notifications. By ensuring that your doctors have access to complete and
+        accurate medical information, we help reduce the risk of misdiagnosis
+        and improve your overall healthcare experience. Enjoy a seamless and
+        convenient healthcare journey with Mediphix.
+      </p>
+    </div>
+  );
 }
 
 export default WhyUs;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,6 +9,9 @@ import FAQ from "../components/landing-page-components/FAQ";
 import JoinWithUs from "../components/landing-page-components/JoinWithUs";
 import Footer from "../components/landing-page-components/Footer";
 import { ConfigProvider } from "antd";
+import featureImg1 from "./../assets/images/landing-page/feature_01.png";
+import featureImg2 from "./../assets/images/landing-page/feature_02.png";
+import featureImg3 from "./../assets/images/landing-page/feature_03.png";
 
 function LandingPage() {
   return (
@@ -36,11 +39,30 @@ function LandingPage() {
         </div>
         {/* Second Section */}
         <Stat />
-        <div className="px-24 bg-red-200">
-          <Feature />
-          <Feature />
-          <Feature />
+        <div className="px-24 bg-mediphix_card_background">
+          <Feature
+            isReverse={false}
+            title="Easy appointment booking"
+            description="Book your appointments with ease and convenience. Our system allows you to search for doctors and medical centers across the island, view their availability, and schedule your visits at a time that suits you best."
+            img={featureImg1}
+          />
+          <Feature
+            isReverse={true}
+            title="Reduced risk of misdiagnosis"
+            description="Ensure accurate diagnoses by providing doctors with your complete medical history. Our secure system stores your medical data, allowing doctors to access past records and make well-informed decisions about your health."
+            img={featureImg2}
+          />
+          <Feature
+            isReverse={false}
+            title="Get Important Reminders"
+            description="Never miss an appointment or important health update. Our notification system sends you timely reminders about your upcoming appointments, medication schedules, and other essential health alerts to keep you on track."
+            img={featureImg3}
+          />
+        </div>
+        <div className="bg-mediphix_card_background px-16 py-8">
           <WhyUs />
+        </div>
+        <div className="px-24 bg-red-200">
           <Feedback />
           <FAQ />
           <JoinWithUs />
