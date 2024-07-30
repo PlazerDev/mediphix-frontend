1392134cfce83459f58ba6a606ff26cb853edcc1
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-07-30 15:05:21 +0530

    link the signup page

--- src/components/landing-page-components/BtnWithBorder.tsx
+++ src/components/landing-page-components/BtnWithBorder.tsx
@@ -1,11 +1,15 @@
 interface Props {
   title: string;
+  onClickFunction?: () => void;
 }
-function BtnWithBorder({ title }: Props) {
+function BtnWithBorder({ title, onClickFunction }: Props) {
   return (
-    <div className="border-2 border-white flex items-center justify-center rounded-md px-4 py-1 text-white hover:bg-white hover:text-mediphix_accent hover:cursor-pointer">
+    <button
+      onClick={onClickFunction}
+      className="border-2 border-white flex items-center justify-center rounded-md px-4 py-1 text-white hover:bg-white hover:text-mediphix_accent hover:cursor-pointer"
+    >
       {title}
-    </div>
+    </button>
   );
 }
 
